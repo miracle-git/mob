ed51e279f70daf6dddee82f9bcf314269199b2b6
Miracle He <hmiinyu@sina.com>
2021-09-29 03:04:51 +0800

    搭建优购uni项目

---
+++ uni-shop-app/main.js
@@ -0,0 +1,25 @@
+
+// #ifndef VUE3
+import Vue from 'vue'
+import App from './app'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import App from './app'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
